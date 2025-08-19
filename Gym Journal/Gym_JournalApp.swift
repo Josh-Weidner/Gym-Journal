2d1db1dcb3e146ea369a2e752a009623465c412e
Josh-Weidner <josh.weidner2@gmail.com>
2025-08-18 21:59:54 -0600

    Initial Commit

---
+++ Gym Journal/Gym_JournalApp.swift
@@ -0,0 +1,32 @@
+//
+//  Gym_JournalApp.swift
+//  Gym Journal
+//
+//  Created by Josh Weidner on 8/18/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Gym_JournalApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
