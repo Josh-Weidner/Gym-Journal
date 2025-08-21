cd42e08511b5a378ead33b0d43f10a333ca3f398
Josh-Weidner <josh.weidner2@gmail.com>
2025-08-20 22:25:45 -0600

    Add Horizontal list view

---
+++ Gym Journal/Model/MuscleGroup.swift
@@ -0,0 +1,8 @@
+//
+//  MuscleGroup.swift
+//  Gym Journal
+//
+//  Created by Josh Weidner on 8/20/25.
+//
+
+import Foundation
---
+++ Gym Journal/Model/Workout.swift
@@ -0,0 +1,8 @@
+//
+//  Workout.swift
+//  Gym Journal
+//
+//  Created by Josh Weidner on 8/20/25.
+//
+
+import Foundation
