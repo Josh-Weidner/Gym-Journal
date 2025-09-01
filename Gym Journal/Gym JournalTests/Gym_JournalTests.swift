868a46ac1c399f1f151c8b5dbc9b5257210b2ac7
Josh-Weidner <josh.weidner2@gmail.com>
2025-08-31 23:12:09 -0600

    We have the app on the phone and we have a create workout feature!

---
+++ Gym Journal/Gym JournalTests/Gym_JournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_JournalTests.swift
+//  Gym JournalTests
+//
+//  Created by Josh Weidner on 8/18/25.
+//
+
+import Testing
+@testable import Gym_Journal
+
+struct Gym_JournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Gym Journal/Views/Workout Detail/AddWorkoutView.swift
@@ -0,0 +1,18 @@
+//
+//  AddWorkoutView.swift
+//  Gym Journal
+//
+//  Created by Josh Weidner on 8/31/25.
+//
+
+import SwiftUI
+
+struct AddWorkoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddWorkoutView()
+}
